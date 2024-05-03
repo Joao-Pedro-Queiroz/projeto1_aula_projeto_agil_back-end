f35c0af16218bebb8f22af95f9ddb81fb7334640
Joao-Pedro-Queiroz <jpqv0105@gmail.com>
2024-05-03 10:57:34 -0300

    código refatorado

--- app.py
+++ app.py
@@ -45,14 +45,58 @@ def get_current_user():
     return mongo.db.usuarios.find_one({"email": auth.email})
 
 
-##### FUNÇÕES DO TIPO GET #####
+# CRUD USUÀRIO
 
-#CHECK
+@app.route('/usuarios', methods=['POST'])
+# Função para adicionar um novo usuário
+def adicionar_usuario():
+    # Obter os dados do corpo da solicitação
+    usuario = request.json
+    nome = usuario.get("nome", "")
+    email = usuario.get("email", "")
+    password = usuario.get("password", "")
+    curso = usuario.get("curso", "")
+    data_nascimento = usuario.get("data_nascimento", "")
+    cpf = usuario.get("cpf", "")
+    entidades = usuario.get("entidades", [])
+    interesses = usuario.get("interesses", [])
+    periodo = usuario.get("periodo", "")
+    projetos = usuario.get("projetos", [])
+
+    # Verificar se os campos obrigatórios foram fornecidos
+    if not nome or not email or not password or not curso or not data_nascimento or not cpf or not periodo or not interesses:
+        return {"error": "Nome, usuario, email, password, curso, data_nascimento, cpf, id e periodo são obrigatórios"}, 400
+    
+    # Verificar se o CPF já existe no banco de dados
+    if mongo.db.usuarios.find_one(filter={"cpf": cpf}):
+        return {"error": "Usuário já existe"}, 409
+    
+    # Gerar um hash SHA-256 da senha
+    hashed_password = hash_password(password)
+    
+    # Criar um dicionário contendo os dados do usuário
+    usuario = {
+                "nome": nome, 
+                "email": email, 
+                "password": hashed_password, 
+                "curso": curso, 
+                "data_nascimento": data_nascimento, 
+                "cpf": cpf,  
+                "periodo": periodo, 
+                "entidades": entidades, 
+                "interesses": interesses, 
+                "projetos": projetos
+            }
+    
+    # Insere o usuário no banco de dados MongoDB
+    mongo.db.usuarios.insert_one(usuario)
+    # Retorna uma mensagem de sucesso e o código de status 201 (Criado)
+    return jsonify({"msg": "Usuário criado com sucesso!"}), 201
+
+
 @app.route('/usuarios', methods=['GET'])
-@requires_auth
 def get_all_users():
-    filtro = {
-    }
+    filtro = {}
     # Define uma projeção para não incluir o campo "_id" nos resultados
     projecao = {"_id": 0}
     # Recupera os dados dos usuários do banco de dados MongoDB usando o filtro e a projeção definidos
@@ -64,27 +108,8 @@ def get_all_users():
     # Retorna a resposta JSON e o código de status 200 (OK)
     return resp, 200
 
-#CHECK
-@app.route('/entidades', methods=['GET'])
-def get_all_entidades():
-    filtro = {
-    }
-    # Define uma projeção para não incluir o campo "_id" nos resultados
-    projecao = {"_id": 0}
-    # Recupera os dados dos usuários do banco de dados MongoDB usando o filtro e a projeção definidos
-    dados_entidades = mongo.db.entidades.find(filtro, projecao)
-    # Cria uma resposta JSON contendo os usuários encontrados
-    resp = {
-        "entidades": list(dados_entidades),
-    }
-    # Retorna a resposta JSON e o código de status 200 (OK)
-    return resp, 200
 
-
-# Função que busca usuarios através do campo "nome"
-#CHECK
 @app.route('/usuarios/<string:nome>', methods=['GET'])
-@requires_auth
 def get_user_by_name(nome):
     # Define um filtro para encontrar o usuário com o nome especificado
     filtro = {
@@ -101,59 +126,8 @@ def get_user_by_name(nome):
     # Retorna a resposta JSON e o código de status 200 (OK)
     return resp, 200
 
-# Função que busca entidades através do campo "nome"
-#CHECK
-@app.route('/entidades/<string:nome>', methods=['GET'])
-def get_entidade_by_name(nome):
-    # Define um filtro para encontrar a entidade com o nome especificado
-    filtro = {
-        "nome": nome
-    }
-    # Define uma projeção para não incluir o campo "_id" nos resultados
-    projecao = {"_id": 0}
-    # Recupera os dados da entidade do banco de dados MongoDB usando o filtro e a projeção definidos
-    dados_entidades = mongo.db.entidades.find(filtro, projecao)
-    # Cria uma resposta JSON contendo a entidade encontrada
-    resp = {
-        "entidade": list(dados_entidades),
-    }
-    # Retorna a resposta JSON e o código de status 200 (OK)
-    return resp, 200
 
-
-##### FUNÇÕES DO TIPO DELETE #####
-#check
-@app.route('/usuarios/<string:nome>', methods=['DELETE'])
-@requires_auth
-def remover_usuario(nome):
-    # Define um filtro para encontrar o usuário com CPF "12345678901"
-    filtro = {
-        "nome": nome
-    }
-    # Remove o usuário do banco de dados MongoDB
-    mongo.db.usuarios.delete_one(filtro)
-    # Retorna uma mensagem de sucesso e o código de status 200 (OK)
-    return {"mensagem": "Usuário removido com sucesso"}, 200
-
-#chcek
-@app.route('/entidades/<string:nome>', methods=['DELETE'])
-@requires_auth
-def remover_entidade(nome):
-    # Define um filtro para encontrar a entidade com CNPJ "12345678901234"
-    filtro = {
-        "nome": nome
-    }
-    # Remove a entidade do banco de dados MongoDB
-    mongo.db.entidades.delete_one(filtro)
-    # Retorna uma mensagem de sucesso e o código de status 200 (OK)
-    return {"mensagem": "Entidade removida com sucesso"}, 200
-
-
-##### FUNÇÕES DO TIPO PUT #####
-
-#chcek
 @app.route('/usuarios/<string:email>', methods=['PUT'])
-@requires_auth
 def editar_usuario(email):
     # Define um filtro para encontrar o usuário com o e-mail especificado
     filtro = {"email": email}
@@ -180,9 +154,97 @@ def editar_usuario(email):
                 return {"erro": "Dados inválidos"}, 400
             return {"mensagem": "Usuário atualizado com sucesso"}, 200
 
-#check
+
+@app.route('/usuarios/<string:nome>', methods=['DELETE'])
+def remover_usuario(nome):
+    # Define um filtro para encontrar o usuário com CPF "12345678901"
+    filtro = {
+        "nome": nome
+    }
+    # Remove o usuário do banco de dados MongoDB
+    mongo.db.usuarios.delete_one(filtro)
+    # Retorna uma mensagem de sucesso e o código de status 200 (OK)
+    return {"mensagem": "Usuário removido com sucesso"}, 200
+
+# CRUD ENTIDADES
+
+@app.route('/entidades', methods=['POST'])
+# Função para adicionar uma nova entidade
+def adicionar_entidade():
+    # Obter os dados do corpo da solicitação
+    nome = request.json.get("nome", "")
+    email = request.json.get("email", "")
+    password = request.json.get("password", "")
+    apresentacao = request.json.get("apresentacao", "")
+    area_atuacao = request.json.get("area_atuacao", "")
+    data_criacao = request.json.get("data_criacao", "")
+    info_contato = request.json.get("info_contato", {})
+    presidente = request.json.get("presidente", "")
+    projetos = request.json.get("projetos", [])
+    vice_presidente = request.json.get("vice_presidente", "")
+
+    # Verificar se os campos obrigatórios foram fornecidos
+    if not nome or not email or not password or not apresentacao or not area_atuacao or not data_criacao or not presidente or not vice_presidente:
+        return {"error": "Nome, email, password, apresentacao, area_atuacao, data_criacao, id, presidente e vice_presidente são obrigatórios"}, 400
+    
+    # Verificar se o nome já existe no banco de dados
+    if mongo.db.entidades.find_one(filter={"nome": nome}):
+        return {"error": "Esse nome já existe"}, 409
+    
+    # Criar um dicionário contendo os dados da entidade
+    entidade = {
+            "email": email,
+            "password": password,
+            "nome": nome,
+            "apresentacao": apresentacao,
+            "area_atuacao": area_atuacao,
+            "data_criacao": data_criacao,
+            "info_contato": info_contato,
+            "presidente": presidente,
+            "projetos": projetos,
+            "vice_presidente": vice_presidente
+        }
+    
+    # Insere a entidade no banco de dados MongoDB
+    mongo.db.entidades.insert_one(entidade)
+    # Retorna uma mensagem de sucesso e o código de status 201 (Criado)
+    return {"mensagem": "Entidade adicionada com sucesso"}, 201
+
+
+@app.route('/entidades', methods=['GET'])
+def get_all_entidades():
+    filtro = {}
+    # Define uma projeção para não incluir o campo "_id" nos resultados
+    projecao = {"_id": 0}
+    # Recupera os dados dos usuários do banco de dados MongoDB usando o filtro e a projeção definidos
+    dados_entidades = mongo.db.entidades.find(filtro, projecao)
+    # Cria uma resposta JSON contendo os usuários encontrados
+    resp = {
+        "entidades": list(dados_entidades),
+    }
+    # Retorna a resposta JSON e o código de status 200 (OK)
+    return resp, 200
+
+
+@app.route('/entidades/<string:nome>', methods=['GET'])
+def get_entidade_by_name(nome):
+    # Define um filtro para encontrar a entidade com o nome especificado
+    filtro = {
+        "nome": nome
+    }
+    # Define uma projeção para não incluir o campo "_id" nos resultados
+    projecao = {"_id": 0}
+    # Recupera os dados da entidade do banco de dados MongoDB usando o filtro e a projeção definidos
+    dados_entidades = mongo.db.entidades.find(filtro, projecao)
+    # Cria uma resposta JSON contendo a entidade encontrada
+    resp = {
+        "entidade": list(dados_entidades),
+    }
+    # Retorna a resposta JSON e o código de status 200 (OK)
+    return resp, 200
+
+
 @app.route('/entidades/<string:email>', methods=['PUT'])
-@requires_auth
 def editar_entidade(email):
     # Define um filtro para encontrar a entidade com o e-mail especificado
     filtro = {"email": email}
@@ -210,92 +272,16 @@ def editar_entidade(email):
             return {"mensagem": "Entidades atualizado com sucesso"}, 200
 
 
-##### FUNÇÕES DO TIPO POST #####
-
-#CHECK
-@app.route('/usuarios', methods=['POST'])
-# Função para adicionar um novo usuário
-def adicionar_usuario():
-    # Obter os dados do corpo da solicitação
-    usuario = request.json
-    nome = usuario.get("nome", "")
-    email = usuario.get("email", "")
-    password = usuario.get("password", "")
-    curso = usuario.get("curso", "")
-    data_nascimento = usuario.get("data_nascimento", "")
-    cpf = usuario.get("cpf", "")
-    entidades = usuario.get("entidades", [])
-    id = usuario.get("id", "")
-    interesses = usuario.get("interesses", [])
-    periodo = usuario.get("periodo", "")
-    projetos = usuario.get("projetos", [])
-    # Verificar se os campos obrigatórios foram fornecidos
-    if not nome or not email or not password or not curso or not data_nascimento or not cpf or not id or not periodo or not interesses:
-        return {"error": "Nome, usuario, email, password, curso, data_nascimento, cpf, id e periodo são obrigatórios"}, 400
-    # Verificar se o CPF já existe no banco de dados
-    if mongo.db.usuarios.find_one(filter={"cpf": cpf}):
-        return {"error": "Usuário já existe"}, 409
-    # Gerar um hash SHA-256 da senha
-    hashed_password = hash_password(password)
-    # Criar um dicionário contendo os dados do usuário
-    usuario = {
-                "nome": nome, 
-                "email": email, 
-                "password": hashed_password, 
-                "curso": curso, 
-                "data_nascimento": data_nascimento, 
-                "cpf": cpf, 
-                "id": id, 
-                "periodo": periodo, 
-                "entidades": entidades, 
-                "interesses": interesses, 
-                "projetos": projetos
-            }
-    # Insere o usuário no banco de dados MongoDB
-    mongo.db.usuarios.insert_one(usuario)
-    # Retorna uma mensagem de sucesso e o código de status 201 (Criado)
-    return jsonify({"msg": "Usuário criado com sucesso!"}), 201
-
-#CHECK
-@app.route('/entidades', methods=['POST'])
-# Função para adicionar uma nova entidade
-def adicionar_entidade():
-    # Obter os dados do corpo da solicitação
-    nome = request.json.get("nome", "")
-    email = request.json.get("email", "")
-    password = request.json.get("password", "")
-    apresentacao = request.json.get("apresentacao", "")
-    area_atuacao = request.json.get("area_atuacao", "")
-    data_criacao = request.json.get("data_criacao", "")
-    id = request.json.get("id", "")
-    info_contato = request.json.get("info_contato", {})
-    presidente = request.json.get("presidente", "")
-    projetos = request.json.get("projetos", [])
-    vice_presidente = request.json.get("vice_presidente", "")
-    # Verificar se os campos obrigatórios foram fornecidos
-    if not nome or not email or not password or not apresentacao or not area_atuacao or not data_criacao or not id or not presidente or not vice_presidente:
-        return {"error": "Nome, email, password, apresentacao, area_atuacao, data_criacao, id, presidente e vice_presidente são obrigatórios"}, 400
-    # Verificar se o nome já existe no banco de dados
-    if mongo.db.entidades.find_one(filter={"nome": nome}):
-        return {"error": "Esse nome já existe"}, 409
-    # Criar um dicionário contendo os dados da entidade
-    entidade = {
-            "email": email,
-            "password": password,
-            "nome": nome,
-            "apresentacao": apresentacao,
-            "area_atuacao": area_atuacao,
-            "data_criacao": data_criacao,
-            "id": id,
-            "info_contato": info_contato,
-            "presidente": presidente,
-            "projetos": projetos,
-            "vice_presidente": vice_presidente
-        }
-    # Insere a entidade no banco de dados MongoDB
-    mongo.db.entidades.insert_one(entidade)
-    # Retorna uma mensagem de sucesso e o código de status 201 (Criado)
-    return {"mensagem": "Entidade adicionada com sucesso"}, 201
+@app.route('/entidades/<string:nome>', methods=['DELETE'])
+def remover_entidade(nome):
+    # Define um filtro para encontrar a entidade com CNPJ "12345678901234"
+    filtro = {
+        "nome": nome
+    }
+    # Remove a entidade do banco de dados MongoDB
+    mongo.db.entidades.delete_one(filtro)
+    # Retorna uma mensagem de sucesso e o código de status 200 (OK)
+    return {"mensagem": "Entidade removida com sucesso"}, 200
 
 
 @app.route('/senha', methods=['GET'])
@@ -321,18 +307,6 @@ def solicitar_recuperacao():
     except:
         return {"erro": "Erro no sistema"}, 500
 
-# Rota pública que não requer autenticação
-@app.route('/')
-def public_route():
-    """Rota pública que não requer autenticação."""
-    return {"msg": "Página pública"}
-
-# Rota protegida que requer autenticação
-@app.route('/secret')
-@requires_auth
-def secret_page():
-    """Rota protegida que requer autenticação."""
-    return {"msg": "Você está autenticado e pode acessar esta página protegida"}
 
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0")
